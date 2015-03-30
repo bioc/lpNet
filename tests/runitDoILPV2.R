ab2f93f6cf696bcbbd3b545ce25897b66813b2c0
Bettina Knapp <bettina.knapp@helmholtz-muenchen.de>
2015-03-30 18:45:49 +0000

    had not previously added alternative to current doILP_steadyState (mistake)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/lpNet@101604 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/runitDoILPV2.R
@@ -0,0 +1,74 @@
+.setUp <- function(){
+	
+	n <<- 3
+	K <<- 4
+	
+	T_nw <<- matrix(c(0,1,0,
+									 0,0,1,
+									 0,0,0), nrow=n, ncol=n, byrow=TRUE)
+	b <<- c(0,1,1,
+				 1,0,1,
+				 1,1,0,
+				 1,1,1)
+	
+	obs_mat <<- matrix(c(0.56, 0.95, 0.95, 0.95,
+											0.56, 0.56, 0.95, 0.95,
+											0.56, 0.56, 0.56, 0.95), nrow=n, ncol=K, byrow=TRUE)
+											
+	lambda <<- 1/10
+	annot <<- getEdgeAnnot(n)
+}
+
+
+test.doILPV2ShortExamplePerGene <- function() {
+
+	true_result_objval <- 2.344474
+	true_result_solution <- c(0.0000000, 0.7947368, 0.0000000, 
+														0.0000000, 0.0000000, 0.7947368, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.7550000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000)
+	
+	delta = rep(0.755, n)
+	delta_type <- "perGene"
+	
+	res <- doILP(obs_mat, delta, lambda, b, n, K, T_=NULL, annot, delta_type,prior=NULL, sourceNode=NULL, sinkNode=NULL, all.int=FALSE, all.pos=FALSE, flag_v2=TRUE)
+
+	checkEquals(true_result_objval, res$objval, tolerance=0.00001)
+	checkEquals(true_result_solution, res$solution, tolerance=0.00001)
+}
+
+
+test.doILPV2ShortExamplePerGeneExp <- function() {
+
+	true_result_objval <- 9.894474
+	true_result_solution <- c(0.0000000, 0.7947368, 0.0000000, 
+														0.0000000, 0.0000000, 0.7947368, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.7550000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.0000000, 
+														0.7550000, 0.0000000, 0.0000000, 
+														0.0000000, 0.0000000, 0.00000000)
+
+	delta = matrix(c(0.755, 0.755, 0.96, 0.755, 
+									 0.755, 0.755, 0.96, 0.755,
+									 0.755, 0.755, 0.96, 0.755), nrow=n, ncol=K, byrow=TRUE)
+
+	delta_type <- "perGeneExp"
+	
+	res <- doILP(obs_mat, delta, lambda, b, n, K, T_=NULL, annot, delta_type, prior=NULL, sourceNode=NULL, sinkNode=NULL, all.int=FALSE, all.pos=FALSE, flag_v2=TRUE)
+	
+	checkEquals(true_result_objval, res$objval, tolerance=0.00001)
+	checkEquals(true_result_solution, res$solution, tolerance=0.00001)
+}
+
